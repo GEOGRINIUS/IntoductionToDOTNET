98ca86274cbdd161d06e2715b86a9f07b2e9b1c6
Gameboy <Gameboy@DESKTOP-M5JQ5GR>
2026-05-20 12:22:02 +0400

    Add project files.

---
+++ Introduction/Program.cs
@@ -0,0 +1,43 @@
+﻿//#define CONSOLE
+//#define STRING_OPERATORS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+#if CONSOLE
+            Console.Title = "introduction to .NET";
+            Console.WriteLine("Hello .NET");
+            Console.WriteLine();
+            Console.CursorLeft = 32;
+            Console.CursorTop = 16;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Cursor position");
+            Console.ResetColor();
+            Console.Beep(37, 2000); 
+#endif
+
+#if STRING_OPERATORS
+            Console.WriteLine("Введите имя и фамилию:");
+            string first_name = Console.ReadLine();
+            string last_name = Console.ReadLine();
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(first_name + " " + last_name + " " + age); //Конкатенация строк
+            Console.WriteLine(string.Format("{0} {1} {2}", first_name, last_name, age)); //Формартирование строк
+            Console.WriteLine($"{first_name} {last_name} {age}"); //
+#endif
+
+
+
+        }
+    }
+}
